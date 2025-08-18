6e3846aedbc9562abad3617dd22fb5233486157a
MengAiDev <mengai@example.com>
2025-08-18 21:22:48 +0800

    Remove build artifacts and update .gitignore
    Co-authored-by: dakou <dakou@iflow.cn>

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/admin/workspace/cli-file-explorer
-# Build directory: /home/admin/workspace/cli-file-explorer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(FileSystemTest "/home/admin/workspace/cli-file-explorer/build/FileSystemTest")
-set_tests_properties(FileSystemTest PROPERTIES  _BACKTRACE_TRIPLES "/home/admin/workspace/cli-file-explorer/CMakeLists.txt;48;add_test;/home/admin/workspace/cli-file-explorer/CMakeLists.txt;0;")
